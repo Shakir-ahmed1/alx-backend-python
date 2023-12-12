0077aa8d6eb00470768f8dbc74337075b241f3d5
Shakir Ahmedsalih <shakirahmedsalih35756@gmail.com>
2023-12-12 23:42:34 +0000

    fixing annotation and pycodestyle

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
 """ Run time for four parallel comprehensions """
 import asyncio
-async_comprehension = __import__('1-async_comprehension').async_comprehension
 from time import time
+async_comprehension = __import__('1-async_comprehension').async_comprehension
 
-async def measure_runtime() -> int:
+
+async def measure_runtime() -> float:
     """ returns the time asyncio.gather taken to run the asyncs"""
     start = time()
     await asyncio.gather(async_comprehension(),
-                   async_comprehension(),
-                   async_comprehension(),
-                   async_comprehension())
+                         async_comprehension(),
+                         async_comprehension(),
+                         async_comprehension())
     end = time()
     return end - start
