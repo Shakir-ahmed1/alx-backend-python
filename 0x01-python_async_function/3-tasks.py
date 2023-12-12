87b0caac1fab49b6084a2b0a09ea380d18713420
Shakir Ahmedsalih <shakirahmedsalih35756@gmail.com>
2023-12-12 20:30:57 +0000

    creating tasks

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" creates a task instances """
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int):
+    """ returns a task instance of wait_random function """
+    return asyncio.Task(wait_random(max_delay))
+
