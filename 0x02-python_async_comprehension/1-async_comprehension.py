84e874a3e019dbcc47dd99923f90ee822d9390c9
Shakir Ahmedsalih <shakirahmedsalih35756@gmail.com>
2023-12-12 23:29:00 +0000

    fixing annotation

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -5,6 +5,6 @@ from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> List[int]:
+async def async_comprehension() -> List[float]:
     """ async comprehension that returns list of floats"""
     return [a async for a in async_generator()]
