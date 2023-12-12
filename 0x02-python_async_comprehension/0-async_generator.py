d8acd725056bb022674f16480befcfe3ecc0271a
Shakir Ahmedsalih <shakirahmedsalih35756@gmail.com>
2023-12-12 23:13:18 +0000

    fixing annotation

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,13 +2,11 @@
 """ Async Generator module """
 from random import uniform
 import asyncio
-from typing import AsyncGenerator
-from time import sleep
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """ generates one value each time"""
-    for i in range(10):
-        asyncio.create_task(asyncio.sleep(1))
-        num = uniform(0, 10)
-        yield num
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield uniform(0, 10)
