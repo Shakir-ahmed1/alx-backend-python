1af87683653f45724ed577ca4e1007e902486348
Shakir Ahmedsalih <shakirahmedsalih35756@gmail.com>
2023-12-12 20:15:30 +0000

    using as_completed, future

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,18 +1,15 @@
 #!/usr/bin/env python3
 """ waits multiple random interval"""
+import asyncio
 from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """ a function that returns a list of waited time"""
+    tasks = [wait_random(max_delay) for _ in range(n)]
     result = []
-    for a in range(n):
-        temp = await wait_random(max_delay)
-        if not result:
-            result.append(temp)
-        for b in range(len(result)):
-            if result[b] > temp:
-                result.insert(b, temp)
-                break
+    for future in asyncio.as_completed(tasks):
+        temp = await future
+        result.append(temp)
     return result
