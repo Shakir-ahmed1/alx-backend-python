66a134a4c4ae0e687cf97caf73542cef3f922b45
Shakir Ahmedsalih <shakirahmedsalih35756@gmail.com>
2024-01-24 08:07:31 +0000

    submit test_utils

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+""" testing case for acces_nasted_map in utils """
+from utils import access_nested_map
+import unittest
+from parameterized import parameterized
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """ access nested map testing case """
+    @parameterized.expand(
+            [
+                ({"a": 1}, ("a",), 1),
+                ({"a": {"b": 2}}, ("a",), {"b": 2}),
+                ({"a": {"b": 2}}, ("a", "b"), 2)
+            ]
+    )
+    def test_access_nasted_map(self, nasted_map, path, expected):
+        """ a test method """
+        self.assertEqual(access_nested_map(nasted_map, path), expected)
+
+
+if __name__ == "__main__":
+    unittest.main()
